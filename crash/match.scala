fb93ad5aa291f603ac5ee53077b000e669c23b94
Charlie Shaw <charliecpshaw@gmail.com>
2026-07-20 12:51:04 +0100

    feat (crash): minimse example

--- crash/match.scala
+++ crash/match.scala
@@ -1,8 +1,18 @@
 //> using scala 3.8.4
 
-@main def main = {
-  val extractor = Extractor[Unit]
-  "" match {
-    case extractor() =>
-  }
+type Unapply[A] = A match {
+  case Unit => Boolean
+  case _    => Option[A]
 }
+
+class Extractor[A] {
+  def unapply(s: String): Unapply[A] = ???
+}
+
+// @main
+// def main = {
+//   val extractor = Extractor[Unit]
+//   "" match {
+//     case extractor(_) =>
+//   }
+// }
